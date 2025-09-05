b9ed1ea98d8e42665fb7ffbccdf99129e74722e4
Warren Goldman <57532603+warrengoldman@users.noreply.github.com>
2025-09-05 11:58:32 -0500

    Set hello world back, added healthy and head calls

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    return "<h1>Hello, World - Delta!</h1>"
+    return "<h1>Hello, World!</h1>"
+#from fastapi.staticfiles import StaticFiles
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+@app.get("/healthy")
+async def health_check():
+    """
+    Handles GET requests to the root path.
+    """
+    return {'status': 'Healthy'}
+
+@app.head("/")
+async def head_root():
+    """
+    Handles HEAD requests to the root path.
+    Returns an empty response with a 200 OK status.
+    """
+    return Response(status_code=200)
