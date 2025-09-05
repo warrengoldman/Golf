2dcf8f2e503980df9e78ff4c0b28e3502ff0e8f0
Warren Goldman <57532603+warrengoldman@users.noreply.github.com>
2025-09-05 13:02:21 -0500

    Adding event name

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Depends
 from fastapi.responses import HTMLResponse
+#from ..database import SessionLocal
+#from typing import Annotated
+#from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+
+#
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+#db_dependency = Annotated[Session, Depends(get_db)]
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>TODO Add page to create event. Also could list existing possible events which are clickable</h1>"
 #from fastapi.staticfiles import StaticFiles
 #app.mount("/static", StaticFiles(directory="static"), name="static")
 
+@app.get("/{event_name}")
+async def display_event(event_name: str):
+    """
+    Handles GET requests to the /{event_name} path.
+    Will retrieve data for event_name from the database
+    To be implemented in the future:
+        If no data exists for event_name, redirect to root path with message set to "Event not found".
+        If data exists, render a page with the event data.
+    Currently, it just returns the event_name.
+    """
+    return {'event_name': event_name}
 
 
 @app.get("/healthy")
